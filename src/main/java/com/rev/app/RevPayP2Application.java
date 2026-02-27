f1390e98fddbd4189da2120201141ed50c4633af
wraith19 <svammsi0901@gmail.com>
2026-02-27 10:17:28 +0530

    Configure application basic environment properties

---
+++ src/main/java/com/rev/app/RevPayP2Application.java
@@ -0,0 +1,13 @@
+package com.rev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevPayP2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevPayP2Application.class, args);
+    }
+
+}
